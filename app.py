b8e7e0e36ce37a97f94b400aa68cb0e8b4d7cbbc
Peerzada Zeeshan <Xeeshu13@gmail.com>
2025-04-22 21:25:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,4 +201,5 @@ def send_text_message(phone, message):
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
